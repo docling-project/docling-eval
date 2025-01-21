801f1ac5ca8e2f4b5c4c27753cf825c5d5480b55
Nikos Livathinos <nli@zurich.ibm.com>
2025-01-21 14:02:01 +0100

    fix: Refactor/improve the code to save log files with evalution tables and png files with the plots and ensure to produce all the evalutions/visualizations in the docs/examples/benchmark_xxx.py files
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

--- docs/examples/benchmark_dpbench.py
+++ docs/examples/benchmark_dpbench.py
@@ -18,14 +18,14 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 def main():
 
     idir = Path(f"./benchmarks/{BenchMarkNames.DPBENCH.value}-original")
 
-    logger.info("Download the DP-Bench dataset")
+    log.info("Download the DP-Bench dataset")
     snapshot_download(
         repo_id="upstage/dp-bench",
         repo_type="dataset",
@@ -43,20 +43,20 @@ def main():
     image_scale = 2.0
 
     if True:
-        logger.info("Create the end-to-end converted DP-Bench dataset")
+        log.info("Create the end-to-end converted DP-Bench dataset")
         create_dpbench_e2e_dataset(
             dpbench_dir=idir, output_dir=odir_lay, image_scale=image_scale, do_viz=True
         )
 
         # Layout
-        logger.info("Evaluate the layout for the DP-Bench dataset")
+        log.info("Evaluate the layout for the DP-Bench dataset")
         evaluate(
             modality=EvaluationModality.LAYOUT,
             benchmark=BenchMarkNames.DPBENCH,
             idir=odir_lay,
             odir=odir_lay,
         )
-        logger.info("Visualize the layout for the DP-Bench dataset")
+        log.info("Visualize the layout for the DP-Bench dataset")
         visualise(
             modality=EvaluationModality.LAYOUT,
             benchmark=BenchMarkNames.DPBENCH,
@@ -65,14 +65,14 @@ def main():
         )
 
         # Reading order
-        logger.info("Evaluate the reading-order for the DP-Bench dataset")
+        log.info("Evaluate the reading-order for the DP-Bench dataset")
         evaluate(
             modality=EvaluationModality.READING_ORDER,
             benchmark=BenchMarkNames.DPBENCH,
             idir=odir_lay,
             odir=odir_lay,
         )
-        logger.info("Visualize the reading-order for the DP-Bench dataset")
+        log.info("Visualize the reading-order for the DP-Bench dataset")
         visualise(
             modality=EvaluationModality.READING_ORDER,
             benchmark=BenchMarkNames.DPBENCH,
@@ -81,14 +81,14 @@ def main():
         )
 
         # Markdown text
-        logger.info("Evaluate the markdown text for the DP-Bench dataset")
+        log.info("Evaluate the markdown text for the DP-Bench dataset")
         evaluate(
             modality=EvaluationModality.MARKDOWN_TEXT,
             benchmark=BenchMarkNames.DPBENCH,
             idir=odir_lay,
             odir=odir_lay,
         )
-        logger.info("Visualize the markdown text for the DP-Bench dataset")
+        log.info("Visualize the markdown text for the DP-Bench dataset")
         visualise(
             modality=EvaluationModality.MARKDOWN_TEXT,
             benchmark=BenchMarkNames.DPBENCH,
@@ -97,19 +97,19 @@ def main():
         )
 
     if True:
-        logger.info("Create the tableformer converted DP-Bench dataset")
+        log.info("Create the tableformer converted DP-Bench dataset")
         create_dpbench_tableformer_dataset(
             dpbench_dir=idir, output_dir=odir_tab, image_scale=image_scale
         )
 
-        logger.info("Evaluate the tableformer for the DP-Bench dataset")
+        log.info("Evaluate the tableformer for the DP-Bench dataset")
         evaluate(
             modality=EvaluationModality.TABLEFORMER,
             benchmark=BenchMarkNames.DPBENCH,
             idir=odir_tab,
             odir=odir_tab,
         )
-        logger.info("Visualize the tableformer for the DP-Bench dataset")
+        log.info("Visualize the tableformer for the DP-Bench dataset")
         visualise(
             modality=EvaluationModality.TABLEFORMER,
             benchmark=BenchMarkNames.DPBENCH,
