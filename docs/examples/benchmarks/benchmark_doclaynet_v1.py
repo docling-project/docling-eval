12e7bf5a1bc05d8ca77694dbd3917b615bb55eb9
Yusik Kim <kmyusk@gmail.com>
2025-01-29 11:05:18 +0100

    added benchmark example for doclaynet v1
    Signed-off-by: Yusik Kim <kmyusk@gmail.com>

---
+++ docs/examples/benchmarks/benchmark_doclaynet_v1.py
@@ -0,0 +1,62 @@
+import logging
+import os
+from pathlib import Path
+
+from tabulate import tabulate  # type: ignore
+
+from docling_eval.benchmarks.constants import BenchMarkNames, EvaluationModality
+from docling_eval.benchmarks.doclaynet_v1.create import create_dlnv1_e2e_dataset
+from docling_eval.cli.main import evaluate, visualise
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    odir = Path(f"./benchmarks/{BenchMarkNames.DOCLAYNETV1.value}-dataset")
+
+    os.makedirs(odir, exist_ok=True)
+
+    if True:
+        log.info("Create the end-to-end converted DocLayNetV2 dataset")
+        create_dlnv1_e2e_dataset(output_dir=odir)
+
+        # Layout
+        log.info("Evaluate the layout for the DocLayNet dataset")
+        evaluate(
+            modality=EvaluationModality.LAYOUT,
+            benchmark=BenchMarkNames.DOCLAYNETV1,
+            idir=odir,
+            odir=odir,
+        )
+        log.info("Visualize the layout for the DocLayNet dataset")
+        visualise(
+            modality=EvaluationModality.LAYOUT,
+            benchmark=BenchMarkNames.DOCLAYNETV1,
+            idir=odir,
+            odir=odir,
+        )
+        # Reading order
+        log.info("Evaluate the reading-order for the DocLayNet dataset")
+        evaluate(
+            modality=EvaluationModality.READING_ORDER,
+            benchmark=BenchMarkNames.DOCLAYNETV1,
+            idir=odir,
+            odir=odir,
+        )
+        log.info("Visualize the reading-order for the DocLayNet dataset")
+        visualise(
+            modality=EvaluationModality.READING_ORDER,
+            benchmark=BenchMarkNames.DOCLAYNETV1,
+            idir=odir,
+            odir=odir,
+        )
+
+
+if __name__ == "__main__":
+    main()
