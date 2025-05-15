809a4961b8508eaadfe92331c4aceae58c1aebae
Samved Divekar <scdiveka@us.ibm.com>
2025-05-15 13:45:26 -0700

    bugfix:populate table structures from the correct api response for azure
    Signed-off-by: Samved Divekar <scdiveka@us.ibm.com>

--- tests/test_tables_azure.py
+++ tests/test_tables_azure.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 import pytest
+from dotenv import load_dotenv
 
 from docling_eval.cli.main import evaluate, visualize
 from docling_eval.datamodels.types import BenchMarkNames, EvaluationModality
@@ -20,6 +21,8 @@ logging.getLogger("PIL").setLevel(logging.WARNING)
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("filelock").setLevel(logging.WARNING)
+
+load_dotenv()
 
 
 @pytest.mark.skipif(
@@ -55,6 +58,44 @@ def test_run_fintabnet_builder():
     visualize(
         modality=EvaluationModality.TABLE_STRUCTURE,
         benchmark=BenchMarkNames.FINTABNET,
+        idir=target_path / "eval_dataset",
+        odir=target_path / "evaluations" / EvaluationModality.TABLE_STRUCTURE.value,
+    )
+
+
+@pytest.mark.skipif(
+    IS_CI, reason="Skipping test in CI because the dataset is too heavy."
+)
+def test_run_pubtabnet_builder():
+    target_path = Path(f"./scratch/{BenchMarkNames.PUBTABNET.value}_azure/")
+    azure_provider = AzureDocIntelligencePredictionProvider(
+        do_visualization=True, ignore_missing_predictions=True
+    )
+
+    dataset = FintabNetDatasetBuilder(
+        target=target_path / "gt_dataset",
+        end_index=5,
+    )
+
+    # dataset.retrieve_input_dataset()  # fetches the source dataset from HF
+    dataset.save_to_disk()  # does all the job of iterating the dataset, making GT+prediction records, and saving them in shards as parquet.
+
+    azure_provider.create_prediction_dataset(
+        name=dataset.name,
+        gt_dataset_dir=target_path / "gt_dataset",
+        target_dataset_dir=target_path / "eval_dataset",
+    )
+
+    evaluate(
+        modality=EvaluationModality.TABLE_STRUCTURE,
+        benchmark=BenchMarkNames.PUBTABNET,
+        idir=target_path / "eval_dataset",
+        odir=target_path / "evaluations" / EvaluationModality.TABLE_STRUCTURE.value,
+    )
+
+    visualize(
+        modality=EvaluationModality.TABLE_STRUCTURE,
+        benchmark=BenchMarkNames.PUBTABNET,
         idir=target_path / "eval_dataset",
         odir=target_path / "evaluations" / EvaluationModality.TABLE_STRUCTURE.value,
     )
