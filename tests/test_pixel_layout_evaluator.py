ed869fc09f001baff78909b88cbed525aa97c2f6
Nikos Livathinos <nli@zurich.ibm.com>
2025-11-14 10:54:44 +0100

    chore: Improve tests for PixelLayoutEvaluator
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

--- tests/test_pixel_layout_evaluator.py
+++ tests/test_pixel_layout_evaluator.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Dict, Optional
 
+import numpy as np
 import pytest
 from docling.datamodel.base_models import ConversionStatus
 from docling_core.types.doc.labels import DocItemLabel
@@ -25,22 +26,66 @@ def test_layout_evaluator():
     # Initialize default evaluator
     eval1 = PixelLayoutEvaluator()
 
-    # Custom label mappings
+    # Peform the evaluation
+    evaluation: DatasetPixelLayoutEvaluation = eval1(test_dataset_dir)
+
+    # Generic assertions
+    assert evaluation is not None
+    for rejection_type, rejection_count in evaluation.rejected_samples.items():
+        assert (
+            rejection_count == 0
+        ), f"Unexpected rejections of type: {rejection_type.value}"
+
+    # Pixel evalution assertions
+    assert evaluation.num_pages == len(evaluation.page_evaluations)
+    detailed_class_names: dict[str, str] = (
+        evaluation.matrix_evaluation.detailed.class_names
+    )
+    num_classes = len(detailed_class_names)
+    confusion_matrix_list = evaluation.matrix_evaluation.detailed.confusion_matrix
+    detailed_confusion_matrix = np.asarray(confusion_matrix_list)
+    assert detailed_confusion_matrix.shape == (
+        num_classes,
+        num_classes,
+    ), "Wrong detailed confusion matrix dims"
+    colapsed_confusion_matrix_list = (
+        evaluation.matrix_evaluation.colapsed.confusion_matrix
+    )
+    colapsed_confusion_matrix = np.asarray(colapsed_confusion_matrix_list)
+    assert colapsed_confusion_matrix.shape == (
+        2,
+        2,
+    ), "Wrong colapsed confusion matrix dims"
+
+    # Save the evaluation
+    pixel_save_root = Path(
+        "scratch/DPBench/evaluations/layout/pixel_layout_evaluations"
+    )
+    eval1.save_evaluations(
+        BenchMarkNames.DPBENCH,
+        evaluation,
+        pixel_save_root,
+    )
+    expected_json_fn = pixel_save_root / "evaluation_DPBench_pixel_layout.json"
+    expected_excel_fn = pixel_save_root / "evaluation_DPBench_pixel_layout.xlsx"
+    assert expected_json_fn.is_file(), "Missing evaluation json file"
+    assert expected_excel_fn.is_file(), "Missing evaluation excel file"
+
+    # Initialize with custom label mappings
     label_mapping: Optional[Dict[DocItemLabel, Optional[DocItemLabel]]] = {
         DocItemLabel.CAPTION: DocItemLabel.TITLE,
         DocItemLabel.DOCUMENT_INDEX: None,
     }
     eval2 = PixelLayoutEvaluator(label_mapping=label_mapping)
+    assert len(eval2._matrix_doclabelitem_to_id) + 1 == len(eval2._matrix_id_to_name)
+    assert (
+        eval2._matrix_doclabelitem_to_id[DocItemLabel.CAPTION]
+        == eval2._matrix_doclabelitem_to_id[DocItemLabel.TITLE]
+    ), "Wrong label mapping in _matrix_doclabelitem_to_id"
+    assert (
+        DocItemLabel.CAPTION.value not in eval2._matrix_id_to_name.values()
+    ), "Wrong label mapping in _matrix_id_to_name"
 
-    # Save the evaluations
-    pixel_ds_evaluation: DatasetPixelLayoutEvaluation = eval1(test_dataset_dir)
-    pixel_save_root: Path = test_dataset_dir / "pixel_layout_evaluations"
-    eval1.save_evaluations(
-        BenchMarkNames.DPBENCH,
-        pixel_ds_evaluation,
-        pixel_save_root,
-    )
 
-
-if __name__ == "__main__":
-    test_layout_evaluator()
+# if __name__ == "__main__":
+#     test_layout_evaluator()
