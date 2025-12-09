21eae304e5bbedf300318ff17d6fd6dc916e8d2c
Nikos Livathinos <nli@zurich.ibm.com>
2025-12-09 13:35:19 +0100

    feat: Update examples bash script to demonstrate visualisations on external predictions
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

--- docs/examples/evaluate_dpbench_on_external_predictions.sh
+++ docs/examples/evaluate_dpbench_on_external_predictions.sh
@@ -38,7 +38,7 @@ evaluate() {
     fi
 
     for modality in "${MODALITIES[@]}"; do
-        echo "Evaluation modality: ${modality}, predictions: ${pred_dir}"
+        echo "Evaluate: modality: ${modality}: predictions: ${pred_dir}"
         uv run docling-eval evaluate \
             --benchmark DPBench \
             --modality "${modality}" \
@@ -49,24 +49,54 @@ evaluate() {
 }
 
 
+visualize() {
+    local pred_dir save_dir modality
+    pred_dir="$1"
+    save_dir="$2"
+
+    # Check if the GT/preds dirs exist
+    if [ ! -d "${GT_DIR}" ]; then
+        echo "Missing GT dir: ${GT_DIR}"
+        exit 1
+    fi
+    if [ ! -d "${pred_dir}" ]; then
+        echo "Missing predictions dir: ${pred_dir}"
+        exit 2
+    fi
+
+    echo "Visualize predictions: ${pred_dir}"
+    uv run docling-eval create_viz \
+        --dataset-dir "${GT_DIR}" \
+        --external-predictions-path "${pred_dir}" \
+        --output-dir "${save_dir}"
+}
+
 ###########################################################################################
 # Main
 #
 
+# Predictions
+
 # json predictions
 evaluate \
     scratch/DPBench/predicted_documents/json \
-    scratch/DPBench/external_evaluations_jsons
+    scratch/DPBench/external_predictions_jsons
 
 
 # doctags predictions
 evaluate \
     scratch/DPBench/predicted_documents/doctag \
-    scratch/DPBench/external_evaluations_doctags
+    scratch/DPBench/external_predictions_doctags
 
 
 # yaml predictions
 evaluate \
     scratch/DPBench/predicted_documents/yaml \
-    scratch/DPBench/external_evaluations_yaml
+    scratch/DPBench/external_predictions_yaml
+
+
+# Visualisations
+visualize \
+    scratch/DPBench/predicted_documents/json \
+    scratch/DPBench/external_predictions_visualisations
 
